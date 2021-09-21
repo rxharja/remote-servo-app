c21a4e578e5e22bce78fdcab8d1a16262d088705
Redon Xharja <redon.xharja@gmail.com>
2021-09-20 20:25:53 -0400

    added React boilerplate code and connected Knob.js

---
+++ FrontEnd/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Knob from "./Knob";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Knob />
+      </header>
+    </div>
+  );
+}
+
+export default App;
