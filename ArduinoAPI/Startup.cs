2a63218f8c47ea66f3a321a4c833c92801712d87
Redon Xharja <redon.xharja@gmail.com>
2021-09-20 20:29:14 -0400

    added startup code and ip port code

--- ArduinoAPI/Startup.cs
+++ ArduinoAPI/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ArduinoAPI.Config;
+using ArduinoAPI.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace ArduinoAPI
@@ -26,8 +21,20 @@ namespace ArduinoAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+		    services.Configure<PortConfiguration>(Configuration.GetSection("SerialSettings"));
+
+            services.AddSingleton<ICommService, SerialService>();
+
+		    services.AddCors(options => {
+				options.AddDefaultPolicy(
+				    builder => builder.AllowAnyHeader()
+                                      .AllowAnyMethod()
+								      .AllowAnyOrigin()
+			    );
+			});
 
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ArduinoAPI", Version = "v1" });
@@ -37,16 +44,18 @@ namespace ArduinoAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ArduinoAPI v1"));
-            }
+            //}
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+		    app.UseCors();
 
             app.UseAuthorization();
 
